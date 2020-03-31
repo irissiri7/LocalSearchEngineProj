91fd214c97e085ab2ee6673ce9ad72d161e7a235
Lydia Lind <lydia.nilsson@gmail.com>
2020-03-31 09:51:44 +0200

    Initial commit
    So I was doing work at the SubmittingFiles branch. I updated it from  master to prepare a pull req. Alex had added a common TestProjectFile and I was gonna copy over my tests from my Tests folder and scrap it afterwards. It got really messed up. Conflicts in the autogenerated files that i could not understand or solve. Tried multiple workarounds but to no use. So I abandoned the SubmittingFiles branch, created a new branch called SubmittingFilesNew and just copied over the actual code i wanted to keep to that one instead. Still I can see in the folders view that there still is this folder Tests which I don't understand where it comes from because I made a brand new branch from master which should have nothing to do with the corrupted old branch SubmittingFiles. But everything works so I decided to let it be now.

---
+++ LocalSearchEngine/TestProject/FilePathVerifierTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ClassLibrary;
+
+
+namespace TestProject
+{
+    public class FilePathVerifierTests
+    {
+        [Test]
+        public void CheckIfValidFilePath_HappyDays_ReturnsTrueAndCorrectMessage()
+        {
+            bool result = FilePathVerifier.CheckIfValidFilepath(@"C:\Users\91lydnil\source\repos\LocalSearchEngineProj\LocalSearchEngine\TestProject\ExampleFiles\ValidTxtFile.txt", ".txt", out string message);
+            Assert.IsTrue(result);
+            Assert.AreEqual("File added", message);
+        }
+
+        [Test]
+        public void CheckIfValidFilePath_FileDoesNotExist_ReturnsFalseAndCorrectMessage()
+        {
+            bool result = FilePathVerifier.CheckIfValidFilepath(@"C:\Users\91lydnil\source\repos\LocalSearchEngineProj\LocalSearchEngine\TestProject\ExampleFiles\IDoNotExistFile.txt", ".txt", out string message);
+            Assert.IsFalse(result);
+            Assert.AreEqual("File does not exist", message);
+        }
+
+        [Test]
+        public void CheckIfValidFilePath_FileIsWrongFormat_ReturnsFalseAndCorrectMessage()
+        {
+            bool result = FilePathVerifier.CheckIfValidFilepath(@"C:\Users\91lydnil\source\repos\LocalSearchEngineProj\LocalSearchEngine\TestProject\ExampleFiles\NotValidTxtFile.xml", ".txt", out string message);
+            Assert.False(result);
+            Assert.AreEqual("File not in right format. Can only process .txt files", message);
+        }
+    }
+}
