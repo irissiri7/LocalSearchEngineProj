60af37cd309cd5d54f0a95fa39aaae46a740576e
Max428 <55417346+Max428@users.noreply.github.com>
2020-03-31 14:04:00 +0200

    Update TextFile.cs

--- LocalSearchEngine/ClassLibrary/TextFile.cs
+++ LocalSearchEngine/ClassLibrary/TextFile.cs
@@ -10,21 +10,57 @@ namespace ClassLibrary
     //This class will represent the .txt files the user wants to work with
     public class TxtFile
     {
+        private List<string> wordList = new List<string>();
         //PROPERTIES
         public string FilePath { get; private set; }
+        public List<String> Words
+        {
+            get { return wordList; }
+            set { wordList = value; }
+        }
 
         //CONSTRUCTOR
         public TxtFile(string filePath)
         {
             FilePath = filePath;
+            GetWords();
         }
 
         //METHODS
-        public string SortWords()
+        private static List<string> SortWords(TxtFile txtFile) //returns a TxtFile sorted
         {
-            Console.WriteLine("Hej");
-            return "Not implemented ";
+            List<string> sortedList = new List<string>();
+            foreach (string s in txtFile.Words)
+            {
+                sortedList.Add(s);
+            }
+            sortedList.Sort();                                   //Implementera egen metod
+
+            return sortedList;
         }
+        private void GetWords()
+        {
+
+            char[] charsToAvoid = { '?', '!', ' ', ',', '.', ':', ';', '\t' };
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                string words = sr.ReadLine();
+                string[] split = words.Split(charsToAvoid);
+                foreach (string s in split)
+                {
+                    wordList.Add(s);
+                }
+            }
+
+        }
+        public void WriteAllWords_ToConsole()
+        {
+            foreach (string s in this.wordList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
 
         public string Search(string word)
         {
